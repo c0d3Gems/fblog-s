43c161d0982c5a561f0b0ac8258262c8df0baba8
cosmin <techdubberstudio@gmail.com>
2021-11-08 11:27:09 +0200

    a

---
+++ internal/pages/crypto/top-10-cryptocurrencies-nov-2021.svelte-aad15950.js
@@ -0,0 +1,123 @@
+import{S as he,i as pe,s as ue,e as r,k as i,j as oe,t as h,J as de,c as s,d as o,n as c,m as ae,a as l,g as p,b as Pt,P as me,H as t,f as q,o as re,I as fe,x as ie,u as se,v as ce}from"../../chunks/vendor-f29ecd34.js";import{H as ye,F as ge}from"../../chunks/Footer-2a32748f.js";function be(le){let d,F,m,U,e,y,z,g,X,L,b,Y,G,v,J,V,k,K,Q,w,Z,tt,$,et,nt,B,ot,at,T,rt,it,E,st,ct,_,lt,ht,C,pt,ut,S,dt,mt,D,ft,yt,A,gt,bt,O,vt,kt,I,wt,$t,M,Bt,Tt,P,Et,_t,x,Ct,St,H,Dt,At,R,Ot,It,N,Mt,j,f,W;return m=new ye({}),f=new ge({}),{c(){d=r("meta"),F=i(),oe(m.$$.fragment),U=i(),e=r("main"),y=r("div"),z=i(),g=r("h1"),X=h("Top 10 Cryptocurrencies to Invest in November 2021"),L=i(),b=r("p"),Y=h(`There's no easy way anymore when it comes to picking a cryptocurrency to invest in. It gets so\r
+		confusing, especially if you're a starter because there are thousands of different\r
+		cryptocurrencies, and tokens available. Which one is legit, which one is a scam? Well, there are\r
+		some key factors you can look into when deciding if a crypto project is a scam or not. But\r
+		first, let's have a look at our top 10 cryptocurrencies we are paying attention to in November\r
+		2021.`),G=i(),v=r("h2"),J=h("1. Bitcoin (BTC) \u2022 Market cap: Over $1.17 Trillion"),V=i(),k=r("p"),K=h(`Created in 2009 by an anonymous person, under the nickname "Satoshi Nakamoto", Bitcoin (BTC) is\r
+		the original crypto. Bitcoin, like most other cryptocurrencies, runs on a distribution system of\r
+		computers, called a "blockchain". This "blockchain" is the entire banking system of the crypto;\r
+		it validates transactions automatically so that people are not needed anymore to do it manually,\r
+		like in a traditional banking system, it keeps the record of your ongoing transactions, your\r
+		current balance in your wallet. You can also contribute to making it more secure, by mining.\r
+		Bitcoin mining is a device, that is connected to the network and becomes part of it. All it\r
+		does, is help process transactions, all for a small fee. That fee is yours to keep. This\r
+		blockchain is what's exciting about Bitcoin and crypto in general because it's a secure, and\r
+		automated system, which manages to keep fraudsters away. Bitcoin's price has skyrocketed as its\r
+		popularity grew. Five years before this day, you could've bought Bitcoin for about $500. Today,\r
+		though, a single Bitcoin is worth over $62000. That's a growth of about 12300%, in five years.`),Q=i(),w=r("h2"),Z=h("2. Ethereum (ETH) \u2022 Market cap: Over $520 Billion"),tt=i(),$=r("p"),et=h(`This one is a bit more complex, but it's more interesting because of that. Why, you ask? well,\r
+		Ethereum is both a cryptocurrency and a blockchain platform. It provides developers with a\r
+		concept of smart contracts, which then can be used for applications. Well, we have a specific\r
+		term for the decentralized apps, we call them "dapps", for short. If you ever heard of NFTs, or\r
+		non-fungible tokens, those are using this mechanism. Ethereum has seen also extreme growth over\r
+		the years. Compared to Bitcoin, its price went from $11 to over $4400, in the last five years, a\r
+		growth of almost 40000%`),nt=i(),B=r("h2"),ot=h("3. Binance Coin (BNB) \u2022 Market cap: Over $88 Billion"),at=i(),T=r("p"),rt=h(`The Binance Coin is a cryptocurrency that you might have used without knowing, if you ever\r
+		traded on Binance, one of the (if not the) biggest crypto exchanges in the world. This coin can\r
+		be either traded, or it is used to pay fees on the Binance platform. Ever since its debut in\r
+		2017, Binance Coin has seen big expansions. It is now used for trading, payment processing, or\r
+		even booking travel arrangements. You can even create your cryptocurrency, using the Binance\r
+		Smart Chain, and you'll have to use Binance Coin to provide liquidity. From 2017 until now,\r
+		Binance Coin has seen a 530000% growth, from $0.10 to over $530.`),it=i(),E=r("h2"),st=h("4. Tether (USDT) \u2022 Market cap: Over $70 Billion"),ct=i(),_=r("p"),lt=h(`Unlike other cryptocurrencies, Tether is what we call, a "stable coin". That means, it is backed\r
+		by fiat currencies like U.S. dollars and the Euro, and, in theory, it keeps a value equal to\r
+		these denominations. Hypothetically, this means Tether's value is consistent than other\r
+		cryptocurrencies, and thus, it's preferred by investors who are scared about the extreme\r
+		fluctuation of other crypto coins.`),ht=i(),C=r("h2"),pt=h("5. Cardano (ADA) \u2022 Market cap: Over $66 Billion"),ut=i(),S=r("p"),dt=h(`This one has a bit of an interesting story, as well. Although it started a bit late in the\r
+		crypto space, Cardano is noticeable for its proof-of-stake validation mechanism. This validation\r
+		mechanism, unlike Bitcoin's and Ethereum's proof-of-work mechanism, decreases energy usage, and\r
+		thus, the impact on the environment by removing the mining process. Cardano also has the smart\r
+		contract capability that Ethereum has, for decentralized applications, and is powered by ADA,\r
+		the native coin of Cardano. Cardano's ADA has seen modest growth, compared to the others, in\r
+		2017, ADA's price was about $0.02, but in 2021, the price is about $2.01 for 1 ADA. In\r
+		percentages, that's an increase of almost 10000%.`),mt=i(),D=r("h2"),ft=h("6. Solana (SOL) \u2022 Market cap: Over $60 Billion"),yt=i(),A=r("p"),gt=h(`Welcome to the decentralized finance (Defi) world! Using decentralized apps (DApps) and smart\r
+		contracts, Solana has come with a new hybrid concept of both proof-of-stake and proof-of-history\r
+		mechanisms, that prove transactions happened quickly and securely. The native token of Solana is\r
+		called SOL. Ever since its beginning in 2020, SOL's price was about $0.77, but by 2021, the\r
+		price got to almost $201. A growth of about 26000%.`),bt=i(),O=r("h2"),vt=h("7. XRP (XRP) \u2022 Market cap: Over $50 Billion"),kt=i(),I=r("p"),wt=h(`Create by some of the same founding people of Ripple, a digital and payment processing company,\r
+		XRP can be used on the Ripple network to facilitate exchanges of different currency types,\r
+		including normal currencies like USD or EUR, but major cryptocurrencies, too. In 2017, XRP\r
+		started by having a price of $0.006. Fast forward into 2021, the price got to $1.08, resulting\r
+		in a rise of 17900%.`),$t=i(),M=r("h2"),Bt=h("8. Polkadot (DOT) \u2022 Market cap: Over $43 Billion"),Tt=i(),P=r("p"),Et=h(`Cryptocurrencies may use some blockchains, but Polkadot wishes to integrate all of them, by\r
+		creating a network of cryptocurrencies that connects to all kinds of blockchains so that they\r
+		can all work together. This concept may change how cryptocurrencies are managed and because of\r
+		it, the growth from 2020 till 2021 was from $2.93 to $44.19, about 1400%.`),_t=i(),x=r("h2"),Ct=h("9. Shiba Inu (SHIB) \u2022 Market Cap: Over $41 Billion"),St=i(),H=r("p"),Dt=h(`Starting in the mid-2020, Shiba Inu is what crypto enthusiasts call a "meme" coin. Shiba Inu\r
+		runs, underneath, on the Ethereum network. The initial price of SHIB was $0.0000000014 and today\r
+		it still trades below a cent but has risen immensely, to $0.00007567, a growth of 5,400,000%.`),At=i(),R=r("h2"),Ot=h("10. Dogecoin (DOGE) \u2022 Market Cap: Over $38 Billion"),It=i(),N=r("p"),Mt=h(`Dogecoin is another "meme" coin, but thanks to celebrities and billionaires like Elon Musk, it\r
+		has gained a lot of popularity and value. Starting as a joke in 2013, Dogecoin rapidly became\r
+		popular thanks to its dedicated community and creative memes. Another interesting fact about\r
+		Dogecoin is that there's an infinite supply of it, but that exposes the coin to susceptible\r
+		devaluation as supply increases. There are petitions signed by Dogecoin fans to make the supply\r
+		limited so that the value will increase, but that decision remains up to the Dogecoin developers\r
+		to take. Nevertheless, Dogecoin's price from 2017 to 2021 has seen a 145000% increase, from\r
+		$0.0002 to $0.29. If you decide to invest in any of these cryptocurrencies, please make your due\r
+		diligence and research. Reading articles like this one is a great start, but not enough. Also,\r
+		be responsible while investing, and don't invest money you can't lose.`),j=i(),oe(f.$$.fragment),this.h()},l(a){const u=de('[data-svelte="svelte-7tvjkx"]',document.head);d=s(u,"META",{name:!0,content:!0}),u.forEach(o),F=c(a),ae(m.$$.fragment,a),U=c(a),e=s(a,"MAIN",{});var n=l(e);y=s(n,"DIV",{class:!0,style:!0}),l(y).forEach(o),z=c(n),g=s(n,"H1",{});var xt=l(g);X=p(xt,"Top 10 Cryptocurrencies to Invest in November 2021"),xt.forEach(o),L=c(n),b=s(n,"P",{});var Ht=l(b);Y=p(Ht,`There's no easy way anymore when it comes to picking a cryptocurrency to invest in. It gets so\r
+		confusing, especially if you're a starter because there are thousands of different\r
+		cryptocurrencies, and tokens available. Which one is legit, which one is a scam? Well, there are\r
+		some key factors you can look into when deciding if a crypto project is a scam or not. But\r
+		first, let's have a look at our top 10 cryptocurrencies we are paying attention to in November\r
+		2021.`),Ht.forEach(o),G=c(n),v=s(n,"H2",{});var Rt=l(v);J=p(Rt,"1. Bitcoin (BTC) \u2022 Market cap: Over $1.17 Trillion"),Rt.forEach(o),V=c(n),k=s(n,"P",{});var Nt=l(k);K=p(Nt,`Created in 2009 by an anonymous person, under the nickname "Satoshi Nakamoto", Bitcoin (BTC) is\r
+		the original crypto. Bitcoin, like most other cryptocurrencies, runs on a distribution system of\r
+		computers, called a "blockchain". This "blockchain" is the entire banking system of the crypto;\r
+		it validates transactions automatically so that people are not needed anymore to do it manually,\r
+		like in a traditional banking system, it keeps the record of your ongoing transactions, your\r
+		current balance in your wallet. You can also contribute to making it more secure, by mining.\r
+		Bitcoin mining is a device, that is connected to the network and becomes part of it. All it\r
+		does, is help process transactions, all for a small fee. That fee is yours to keep. This\r
+		blockchain is what's exciting about Bitcoin and crypto in general because it's a secure, and\r
+		automated system, which manages to keep fraudsters away. Bitcoin's price has skyrocketed as its\r
+		popularity grew. Five years before this day, you could've bought Bitcoin for about $500. Today,\r
+		though, a single Bitcoin is worth over $62000. That's a growth of about 12300%, in five years.`),Nt.forEach(o),Q=c(n),w=s(n,"H2",{});var Ft=l(w);Z=p(Ft,"2. Ethereum (ETH) \u2022 Market cap: Over $520 Billion"),Ft.forEach(o),tt=c(n),$=s(n,"P",{});var Ut=l($);et=p(Ut,`This one is a bit more complex, but it's more interesting because of that. Why, you ask? well,\r
+		Ethereum is both a cryptocurrency and a blockchain platform. It provides developers with a\r
+		concept of smart contracts, which then can be used for applications. Well, we have a specific\r
+		term for the decentralized apps, we call them "dapps", for short. If you ever heard of NFTs, or\r
+		non-fungible tokens, those are using this mechanism. Ethereum has seen also extreme growth over\r
+		the years. Compared to Bitcoin, its price went from $11 to over $4400, in the last five years, a\r
+		growth of almost 40000%`),Ut.forEach(o),nt=c(n),B=s(n,"H2",{});var jt=l(B);ot=p(jt,"3. Binance Coin (BNB) \u2022 Market cap: Over $88 Billion"),jt.forEach(o),at=c(n),T=s(n,"P",{});var Wt=l(T);rt=p(Wt,`The Binance Coin is a cryptocurrency that you might have used without knowing, if you ever\r
+		traded on Binance, one of the (if not the) biggest crypto exchanges in the world. This coin can\r
+		be either traded, or it is used to pay fees on the Binance platform. Ever since its debut in\r
+		2017, Binance Coin has seen big expansions. It is now used for trading, payment processing, or\r
+		even booking travel arrangements. You can even create your cryptocurrency, using the Binance\r
+		Smart Chain, and you'll have to use Binance Coin to provide liquidity. From 2017 until now,\r
+		Binance Coin has seen a 530000% growth, from $0.10 to over $530.`),Wt.forEach(o),it=c(n),E=s(n,"H2",{});var qt=l(E);st=p(qt,"4. Tether (USDT) \u2022 Market cap: Over $70 Billion"),qt.forEach(o),ct=c(n),_=s(n,"P",{});var zt=l(_);lt=p(zt,`Unlike other cryptocurrencies, Tether is what we call, a "stable coin". That means, it is backed\r
+		by fiat currencies like U.S. dollars and the Euro, and, in theory, it keeps a value equal to\r
+		these denominations. Hypothetically, this means Tether's value is consistent than other\r
+		cryptocurrencies, and thus, it's preferred by investors who are scared about the extreme\r
+		fluctuation of other crypto coins.`),zt.forEach(o),ht=c(n),C=s(n,"H2",{});var Xt=l(C);pt=p(Xt,"5. Cardano (ADA) \u2022 Market cap: Over $66 Billion"),Xt.forEach(o),ut=c(n),S=s(n,"P",{});var Lt=l(S);dt=p(Lt,`This one has a bit of an interesting story, as well. Although it started a bit late in the\r
+		crypto space, Cardano is noticeable for its proof-of-stake validation mechanism. This validation\r
+		mechanism, unlike Bitcoin's and Ethereum's proof-of-work mechanism, decreases energy usage, and\r
+		thus, the impact on the environment by removing the mining process. Cardano also has the smart\r
+		contract capability that Ethereum has, for decentralized applications, and is powered by ADA,\r
+		the native coin of Cardano. Cardano's ADA has seen modest growth, compared to the others, in\r
+		2017, ADA's price was about $0.02, but in 2021, the price is about $2.01 for 1 ADA. In\r
+		percentages, that's an increase of almost 10000%.`),Lt.forEach(o),mt=c(n),D=s(n,"H2",{});var Yt=l(D);ft=p(Yt,"6. Solana (SOL) \u2022 Market cap: Over $60 Billion"),Yt.forEach(o),yt=c(n),A=s(n,"P",{});var Gt=l(A);gt=p(Gt,`Welcome to the decentralized finance (Defi) world! Using decentralized apps (DApps) and smart\r
+		contracts, Solana has come with a new hybrid concept of both proof-of-stake and proof-of-history\r
+		mechanisms, that prove transactions happened quickly and securely. The native token of Solana is\r
+		called SOL. Ever since its beginning in 2020, SOL's price was about $0.77, but by 2021, the\r
+		price got to almost $201. A growth of about 26000%.`),Gt.forEach(o),bt=c(n),O=s(n,"H2",{});var Jt=l(O);vt=p(Jt,"7. XRP (XRP) \u2022 Market cap: Over $50 Billion"),Jt.forEach(o),kt=c(n),I=s(n,"P",{});var Vt=l(I);wt=p(Vt,`Create by some of the same founding people of Ripple, a digital and payment processing company,\r
+		XRP can be used on the Ripple network to facilitate exchanges of different currency types,\r
+		including normal currencies like USD or EUR, but major cryptocurrencies, too. In 2017, XRP\r
+		started by having a price of $0.006. Fast forward into 2021, the price got to $1.08, resulting\r
+		in a rise of 17900%.`),Vt.forEach(o),$t=c(n),M=s(n,"H2",{});var Kt=l(M);Bt=p(Kt,"8. Polkadot (DOT) \u2022 Market cap: Over $43 Billion"),Kt.forEach(o),Tt=c(n),P=s(n,"P",{});var Qt=l(P);Et=p(Qt,`Cryptocurrencies may use some blockchains, but Polkadot wishes to integrate all of them, by\r
+		creating a network of cryptocurrencies that connects to all kinds of blockchains so that they\r
+		can all work together. This concept may change how cryptocurrencies are managed and because of\r
+		it, the growth from 2020 till 2021 was from $2.93 to $44.19, about 1400%.`),Qt.forEach(o),_t=c(n),x=s(n,"H2",{});var Zt=l(x);Ct=p(Zt,"9. Shiba Inu (SHIB) \u2022 Market Cap: Over $41 Billion"),Zt.forEach(o),St=c(n),H=s(n,"P",{});var te=l(H);Dt=p(te,`Starting in the mid-2020, Shiba Inu is what crypto enthusiasts call a "meme" coin. Shiba Inu\r
+		runs, underneath, on the Ethereum network. The initial price of SHIB was $0.0000000014 and today\r
+		it still trades below a cent but has risen immensely, to $0.00007567, a growth of 5,400,000%.`),te.forEach(o),At=c(n),R=s(n,"H2",{});var ee=l(R);Ot=p(ee,"10. Dogecoin (DOGE) \u2022 Market Cap: Over $38 Billion"),ee.forEach(o),It=c(n),N=s(n,"P",{});var ne=l(N);Mt=p(ne,`Dogecoin is another "meme" coin, but thanks to celebrities and billionaires like Elon Musk, it\r
+		has gained a lot of popularity and value. Starting as a joke in 2013, Dogecoin rapidly became\r
+		popular thanks to its dedicated community and creative memes. Another interesting fact about\r
+		Dogecoin is that there's an infinite supply of it, but that exposes the coin to susceptible\r
+		devaluation as supply increases. There are petitions signed by Dogecoin fans to make the supply\r
+		limited so that the value will increase, but that decision remains up to the Dogecoin developers\r
+		to take. Nevertheless, Dogecoin's price from 2017 to 2021 has seen a 145000% increase, from\r
+		$0.0002 to $0.29. If you decide to invest in any of these cryptocurrencies, please make your due\r
+		diligence and research. Reading articles like this one is a great start, but not enough. Also,\r
+		be responsible while investing, and don't invest money you can't lose.`),ne.forEach(o),n.forEach(o),j=c(a),ae(f.$$.fragment,a),this.h()},h(){document.title="Top 10 Cryptocurrencies to invest in November 2021 \u2022 Finservio.com",Pt(d,"name","description"),Pt(d,"content","Find out which cryptocurrencies are on our radar in Novermber 2021. Check which cryptocurrencies you should invest in."),Pt(y,"class","cover"),me(y,"background-image","url(/img/top-10-cryptocurrencies-for-november-2021.jpg)")},m(a,u){t(document.head,d),q(a,F,u),re(m,a,u),q(a,U,u),q(a,e,u),t(e,y),t(e,z),t(e,g),t(g,X),t(e,L),t(e,b),t(b,Y),t(e,G),t(e,v),t(v,J),t(e,V),t(e,k),t(k,K),t(e,Q),t(e,w),t(w,Z),t(e,tt),t(e,$),t($,et),t(e,nt),t(e,B),t(B,ot),t(e,at),t(e,T),t(T,rt),t(e,it),t(e,E),t(E,st),t(e,ct),t(e,_),t(_,lt),t(e,ht),t(e,C),t(C,pt),t(e,ut),t(e,S),t(S,dt),t(e,mt),t(e,D),t(D,ft),t(e,yt),t(e,A),t(A,gt),t(e,bt),t(e,O),t(O,vt),t(e,kt),t(e,I),t(I,wt),t(e,$t),t(e,M),t(M,Bt),t(e,Tt),t(e,P),t(P,Et),t(e,_t),t(e,x),t(x,Ct),t(e,St),t(e,H),t(H,Dt),t(e,At),t(e,R),t(R,Ot),t(e,It),t(e,N),t(N,Mt),q(a,j,u),re(f,a,u),W=!0},p:fe,i(a){W||(ie(m.$$.fragment,a),ie(f.$$.fragment,a),W=!0)},o(a){se(m.$$.fragment,a),se(f.$$.fragment,a),W=!1},d(a){o(d),a&&o(F),ce(m,a),a&&o(U),a&&o(e),a&&o(j),ce(f,a)}}}class we extends he{constructor(d){super();pe(this,d,null,be,ue,{})}}export{we as default};
